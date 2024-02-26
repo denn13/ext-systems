3eb3e86f5506d9fbbb6552260e4fcc89f63326b1
denn13 <denisn13@gmail.com>
2024-02-26 23:58:27 +0700

    make some classes for DOA

---
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -0,0 +1,12 @@
+package edu.javacourse.city.dao;
+
+import edu.javacourse.city.domain.PersonRequest;
+import edu.javacourse.city.domain.PersonResponse;
+
+public class PersonCheckDao {
+
+    public PersonResponse chechPerson(PersonRequest request) {
+        
+    }
+
+}
