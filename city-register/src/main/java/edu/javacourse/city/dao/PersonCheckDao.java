2ee797873b720aeada13da9d0fc9dcd4319047f1
denn13 <denisn13@gmail.com>
2024-02-28 21:09:44 +0700

    added person check DAO and exception

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -2,12 +2,37 @@ package edu.javacourse.city.dao;
 
 import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckExeption;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class PersonCheckDao {
 
-    public PersonResponse checkPerson(PersonRequest request) {
-        return new PersonResponse();
-        
+    private static final String SQL_REQUEST = "";
+
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckExeption {
+        PersonResponse response = new PersonResponse();
+
+        try (Connection con = getConnection();
+             PreparedStatement stmt = con.prepareStatement(SQL_REQUEST)) {
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                response.setRegistered(true);
+                response.setTemporal(rs.getBoolean("temporal"));
+            }
+
+        } catch (SQLException ex) {
+            throw new PersonCheckExeption(ex);
+        }
+
+        return response;
+    }
+
+    private Connection getConnection() {
+        return null;
     }
 
 }
