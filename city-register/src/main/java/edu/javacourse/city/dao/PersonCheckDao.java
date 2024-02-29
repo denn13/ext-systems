fc04cd2241a02fc6274d9b0a04da14035384b465
denn13 <denisn13@gmail.com>
2024-02-29 15:25:22 +0700

    added SQL Query in PersonChekDAO

--- city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
+++ city-register/src/main/java/edu/javacourse/city/dao/PersonCheckDao.java
@@ -11,7 +11,18 @@ import java.sql.SQLException;
 
 public class PersonCheckDao {
 
-    private static final String SQL_REQUEST = "";
+    private static final String SQL_REQUEST =
+            "SELECT ap.temporal  FROM cr_address_person ap " +
+            "INNER JOIN cr_address a on a.address_id = ap.address_id " +
+            "INNER JOIN cr_person p on p.person_id = ap.person_id " +
+            "where " +
+            "upper(p.sur_name) = upper(?) " +
+            "and upper(p.given_name) = upper(?) " +
+            "and upper(p.patronymic) = upper(?) " +
+            "and p.date_of_birth = ? " +
+            "and upper(a.building) = upper(?) " +
+            "and upper(a.extension) = upper(?) " +
+            "and upper(a.apartment) = upper(?)";
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckExeption {
         PersonResponse response = new PersonResponse();
