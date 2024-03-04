7489e3ae7a9081fef5ce0f5a3c189c027b29208f
denn13 <denisn13@gmail.com>
2024-03-05 01:01:18 +0700

    begin of multiThread

--- network/src/main/java/edu/javacourse/net/Client.java
+++ network/src/main/java/edu/javacourse/net/Client.java
@@ -4,27 +4,33 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        sendRequest();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            SimpleClient sc = new SimpleClient();
+        }
+}
 
-    }
+class SimpleClient extends Thread{
+    @Override
+    public void run() {
+        try {
+            Socket socket = new Socket("127.0.0.1", 25225);
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-    private static void sendRequest() throws IOException {
-        Socket socket = new Socket("127.0.0.1", 25225);
-        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            String s = "Denis";
 
-        String s = "Denis";
+            bw.write(s);
+            bw.newLine();
+            bw.flush();
 
-        bw.write(s);
-        bw.newLine();
-        bw.flush();
+            String answer = br.readLine();
+            System.out.println("Client got string: " + answer);
 
-        String answer = br.readLine();
-        System.out.println("Client got string: " + answer);
-
-        br.close();
-        bw.close();
-
+            br.close();
+            bw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 }
