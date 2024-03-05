2f17215f37ed9596be95fdb30d060da7668abc78
denn13 <denisn13@gmail.com>
2024-03-05 22:23:58 +0700

    add thread sleep in simple server

--- network/src/main/java/edu/javacourse/net/Server.java
+++ network/src/main/java/edu/javacourse/net/Server.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         ServerSocket socket = new ServerSocket(25225);
 
         System.out.println("Server started: ");
@@ -17,13 +17,15 @@ public class Server {
 
     }
 
-    private static void handleRequest(Socket client) throws IOException {
+    private static void handleRequest(Socket client) throws IOException, InterruptedException {
         BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
 
         StringBuffer sb = new StringBuffer("Hello ");
         String userName = br.readLine();
         System.out.println("Server got string: " + userName);
+        Thread.sleep(2000);
+
         sb.append(userName);
 
         bw.write(sb.toString());
